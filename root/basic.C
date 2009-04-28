b9cb57f43c173da8512e1e385620b1e84b1b70f6
krisse <krisse@001da483-2859-4b5c-b8fd-aac4be6cb3b1>
2009-04-28 13:10:54 +0000

    Skrevet ting til sm teori. Om gauge grupper og begyndt på afsnit om feynman diagrammer.

--- root/basic.C
+++ root/basic.C
@@ -53,29 +53,56 @@ void loadhist() {
 	makehist("../data/v4_gg_g_uu_1220_1520.txt", hist2, lines);
 	makehist("../data/v4_gg_g_uu_1520_1820.txt", hist2, lines);
 	
+	TH1F *hist3 = new TH1F("hist3","uu -> A -> dd",nbins,ecm_start,ecm_end);
+	hist2->GetXaxis()->SetTitle("E [GeV]");
+    hist2->GetYaxis()->SetTitle("cross section [pb]");
+	lines = 0;
+	makehist("../data/v4_uu_a_dd_20_320.txt", hist3, lines);
+	makehist("../data/v4_uu_a_dd_320_620.txt", hist3, lines);
+	makehist("../data/v4_uu_a_dd_620_920.txt", hist3, lines);
+	makehist("../data/v4_uu_a_dd_920_1220.txt", hist3, lines);
+	makehist("../data/v4_uu_a_dd_1220_1520.txt", hist3, lines);
+	makehist("../data/v4_uu_a_dd_1520_1820.txt", hist3, lines);
+	
 	f->Write();
 }
 
-void drawhist() {
-	TCanvas *c1 = new TCanvas("c1","uu -> g -> dd",1000,750);
-	hist1->Draw("e");
-	TCanvas *c2 = new TCanvas("c2","gg -> g -> uu",1000,750);
-	hist2->Draw("e");
+void drawhist(Int_t plotnum) {
+//	if (plotnum = -1) {
+//		printf("Syntax: dividehist(Number of plot to make (1,2), Save the plot to .png (0=no, 1=yes), Name of file )");
+//		printf("Plot 1: uu -> g -> dd, gg -> g -> uu and uu -> A -> dd.");
+//		printf("Plot 2: -1 x 64(gg -> g -> uu) / 9(uu -> g -> dd)");
+//	}
+	
+	if (plotnum == 1) {
+//		clearhist();
+		TCanvas *c1 = new TCanvas("c1","uu -> g -> dd",1000,750);
+		hist1->Draw("e");
+		TCanvas *c2 = new TCanvas("c2","gg -> g -> uu",1000,750);
+		hist2->Draw("e");
+		TCanvas *c3 = new TCanvas("c3","uu -> A -> dd",1000,750);
+		hist3->Draw("e");
+	}
+	
+	else if (plotnum == 2) {
+//		clearhist();
+		TH1F *histdiv = hist1->Clone("histdiv");
+		histdiv->SetTitle(" -1 x 64(gg -> g -> uu) / 9(uu -> g -> dd) ");
+		histdiv->GetXaxis()->SetTitle("GeV");
+    	histdiv->GetYaxis()->SetTitle("ratio");
+		histdiv->Reset();
+		// Divide the two histograms and divide by -9/64,
+		// minus sign from taking only s-channel contribution.
+		histdiv->Divide(hist2,hist1,-64,9);
+		TCanvas *c1 = new TCanvas("c1"," (gg -> g -> uu) / (uu -> g -> dd) ",1000,750);
+		histdiv->Draw("e");
+	}
+	
+	else if (plotnum == 3) {
+	}
 }
-
-void dividehist() {
-//	hist1->Sumw2();
-//	hist2->Sumw2();
-	TH1F *histdiv = hist1->Clone("histdiv");
-	histdiv->SetTitle(" -1 x 64(gg -> g -> uu) / 9(uu -> g -> dd) ");
-	histdiv->GetXaxis()->SetTitle("GeV");
-    histdiv->GetYaxis()->SetTitle("ratio");
-	histdiv->Reset();
-	// Divide the two histograms and divide by -9/64,
-	// minus sign from taking only s-channel contribution.
-	histdiv->Divide(hist2,hist1,-64,9);
-	TCanvas *c3 = new TCanvas("c3"," (gg -> g -> uu) / (uu -> g -> dd) ",1000,750);
-//	histdiv->Sumw2();
-	histdiv->Draw("e");
-//	c3->SaveAs("PDFratio.pdf");
-}
+void clearhist() {
+	delete c1;
+	delete c2;
+	delete c3;
+}	
